cc56925f3c7f9e6833850a2bcdcdc95cff09940a
ant4g0nist <ant4g0nist@users.noreply.github.com>
2023-02-04 11:42:43 +0000

    chore: makes tackle the default sample generator

--- src/core/index.ts
+++ src/core/index.ts
@@ -52,22 +52,6 @@ export const createNewProject = async () => {
       message: 'Do you want to initialize your Drogon project with samples?',
     },
     {
-      type: prev => (prev ? 'multiselect' : null),
-      name: 'sampleGeneratorType',
-      message: 'Which code generator tool do you want to use?',
-      choices: [
-        {
-          title: 'Tackle (https://github.com/sudoblockio/tackle-icon-sc-poc)',
-          value: 'tackle',
-        },
-        {
-          title:
-            'Java SCORE examples(https://github.com/icon-project/java-score-examples)',
-          value: 'javascore',
-        },
-      ],
-    },
-    {
       type: 'toggle',
       name: 'generateAccount',
       initial: true,
@@ -100,15 +84,9 @@ export const createNewProject = async () => {
   await initialiseProject(projectPath);
 
   if (response.createSamples) {
-    if (response.sampleGeneratorType == 'javascore') {
-      const boilerplate = await pickABoilerplate();
-      await scaffoldProject(boilerplate, ICON_TEMPLATES_REPO, projectPath);
-      await initProjectIncludes(projectPath, boilerplate);
-    } else if (response.sampleGeneratorType == 'tackle') {
       const projectName = response.path;
       await runTackle(projectName, projectPath);
     }
-  }
 };
 
 export const pickABoilerplate = async () => {
@@ -135,7 +113,7 @@ export const pickABoilerplate = async () => {
 const initialiseProject = async (path: string) => {
   const name = basename(path);
 
-  fs.mkdirSync(`${path}/src/`, {recursive: true});
+  fs.mkdirSync(`${path}/`, {recursive: true});
 
   const config = Config.generateNew(name);
 
@@ -151,17 +129,6 @@ const initialiseProject = async (path: string) => {
     if (err) panic(`Failed to create .gitignore. ${err}`);
   });
 
-  fs.writeFile(`${path}/build.gradle`, mainBuildGradle, err => {
-    if (err) panic(`Failed to create build.gradle. ${err}`);
-  });
-
-  fs.writeFile(
-    `${path}/settings.gradle`,
-    gradleSettings.replace('java-score-examples', name),
-    err => {
-      if (err) panic(`Failed to create build.gradle. ${err}`);
-    }
-  );
   return name;
 };
 
@@ -202,4 +169,4 @@ export const gradleCommands = async (projectPath: string, args: any) => {
 const mountAndRunGradle = async (projectPath: string, args: any, cb: any) => {
   const command = '/goloop/gradlew --build-cache -g /goloop/app/.cache/';
   mountAndRunCommand(projectPath, args, command, cb);
-};
+}
